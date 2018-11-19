13c850ba66f3def3ac2b9b3ecb5a97afb8b28db0
Ub1t <44301460+Ub1t@users.noreply.github.com>
2018-11-19 20:40:29 +0300

    Update hw_6_2.py

--- hw_6_2.py
+++ hw_6_2.py
@@ -1,17 +1,11 @@
-""" Задание №2.
-Переделать Задание №1 с созданием и использованием собственное исключение
-WhitespaceError с атрибутами:
-    position - позиция в строке
-    symbol - какой именно непечатный символ
-Функция main() демонстрирует работу вашей функции, должна красиво показывать
-что именно вызвало исключение.
-"""
 
 from string import whitespace
 class WhitespaceError(Exception):
     def __init__(self, position, symbol):
         self.position = position
         self.symbol = symbol
+    def posym(self):
+        print("Позиция непечатного символа: {}.".format(self.position), "Символ: '{}'.".format(self.symbol))
 
 
 def string_processing(text, *args, **kwargs):
@@ -19,9 +13,10 @@ def string_processing(text, *args, **kwargs):
     for i in text:
         if i in whitespace:
             try:
-                raise WhitespaceError(loop, i)
-            except WhitespaceError:
-                print("Позиция непечатного символа: {}.".format(loop), "Символ: '{}'.".format(i))
+                raise ValueError
+            except ValueError:
+                s = WhitespaceError(loop, i)
+                s.posym()
         elif loop == len(text):
             result = text.upper()
             return result
